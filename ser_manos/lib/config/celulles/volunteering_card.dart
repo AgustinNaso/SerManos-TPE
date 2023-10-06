e947353728e6996783aede67546bf56fed844877
Agustin Naso <anaso@itba.edu.ar>
2023-10-06 18:37:31 -0300

    feat: basic volunteering card

--- ser_manos/lib/config/celulles/volunteering_card.dart
+++ ser_manos/lib/config/celulles/volunteering_card.dart
@@ -8,15 +8,34 @@ class VolunteeringCard extends StatelessWidget {
     return Center(
       child: Container(
         width: 320,
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white, // Shadow color
+              spreadRadius: 5, // How far the shadow should spread
+              blurRadius: 7, // How blurry the shadow should be
+              offset: const Offset(0, 3), // Offset in the x and y direction
+            ),
+          ],
+        ),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              height: 140,
-              child: Image.asset('assets/images/Imagen.png'),
-            ),
+            Image.asset('assets/images/Imagen.png'),
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: Container(
+                  color: Colors.amber,
+                  width: 200,
+                  child: const Text('Un Techo para mi País',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          letterSpacing: 0.15)),
+                )),
           ],
         ),
       ),
