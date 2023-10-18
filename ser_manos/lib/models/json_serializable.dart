6c812521f5e358a986ea2e223dab5d158c928122
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2023-10-18 19:28:43 -0300

    fix: add unimplemented error

--- ser_manos/lib/models/json_serializable.dart
+++ ser_manos/lib/models/json_serializable.dart
@@ -3,5 +3,7 @@ abstract class JsonSerializable<T> {
   Map<String, dynamic> toJson();
 
   // weird without return type but it works
-  static fromJson(Map<String, dynamic> json) {}
+  static fromJson(Map<String, dynamic> json) {
+    throw UnimplementedError("fromJson not implemented");
+  }
 }
