080d387bad4efc3124ae52c750b98fbd58c1ff74
Agustin Naso <anaso@itba.edu.ar>
2023-10-28 21:01:41 -0300

    feat: shadows. card with right shadows

--- ser_manos/lib/config/cellules/volunteering_card.dart
+++ ser_manos/lib/config/cellules/volunteering_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ser_manos/config/molecules/vacancies/vacancies.dart';
+import 'package:ser_manos/config/tokens/sermanos_box_shadows.dart';
 import 'package:ser_manos/config/tokens/sermanos_colors.dart';
 import 'package:ser_manos/config/tokens/sermanos_typography.dart';
 
@@ -8,74 +9,72 @@ class VolunteeringCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 328,
-        height: 234,
-        decoration: const BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey, // Shadow color
-              spreadRadius: 1, // How far the shadow should spread
-              blurRadius: 7, // How blurry the shadow should be
-              offset: Offset(0, 1), // Offset in the x and y direction
-            ),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/volunteering');
+      },
+      child: Center(
+        child: Container(
+          width: 328,
+          height: 234,
+          decoration: BoxDecoration(
+            boxShadow: Shadows.shadow1,
+          ),
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset('assets/images/Imagen.png'),
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  color: Colors.white,
+                  height: 96,
+                  width: 328,
+                  child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        const Column(
+                          mainAxisSize: MainAxisSize.max,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('ACCION SOCIAL',
+                                style: SermanosTypography.overline(
+                                    color: SermanosColors.neutral75)),
+                            Text('Un Techo para mi Pais',
+                                style: SermanosTypography.subtitle01()),
+                            SizedBox(height: 4),
+                            Vacancies(vacancy: 10)
+                          ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(
+                                Icons.favorite_border,
+                                color: SermanosColors.primary100,
+                              ),
+                              onPressed: () {},
+                            ),
+                            IconButton(
+                              alignment: Alignment.bottomCenter,
+                              icon: const Icon(
+                                Icons.location_on,
+                                color: SermanosColors.primary100,
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ]),
+                ),
+              ]),
         ),
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/images/Imagen.png'),
-              Container(
-                padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
-                color: Colors.white,
-                height: 96,
-                width: 328,
-                child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      Column(
-                        mainAxisSize: MainAxisSize.max,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text('ACCION SOCIAL',
-                              style: SermanosTypography.overline(
-                                  color: SermanosColors.neutral75)),
-                          Text('Un Techo para mi Pais',
-                              style: SermanosTypography.subtitle01()),
-                          const SizedBox(height: 4),
-                          const Vacancies(vacancy: 10)
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(
-                              Icons.favorite_border,
-                              color: SermanosColors.primary100,
-                            ),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            alignment: Alignment.bottomCenter,
-                            icon: const Icon(
-                              Icons.location_on,
-                              color: SermanosColors.primary100,
-                            ),
-                            onPressed: () {},
-                          ),
-                        ],
-                      ),
-                    ]),
-              ),
-            ]),
       ),
     );
   }
