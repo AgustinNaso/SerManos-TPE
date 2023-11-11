c4b2bac8646814f1531981549a3bb2c6b3b48777
Agustin Naso <anaso@itba.edu.ar>
2023-11-11 12:44:44 -0300

    fix: temporary comment

--- ser_manos/lib/providers/postulation_provider.dart
+++ ser_manos/lib/providers/postulation_provider.dart
@@ -1,31 +1,34 @@
-final postulationProvider =
-    StateNotifierProvider<PostulationProvider, PostulationState>((ref) {
-  return PostulationProvider(ref.read);
-});
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:ser_manos/data/models/volunteering_postulation.dart';
 
-class PostulationProvider extends StateNotifier<PostulationState> {
-  final Reader _read;
+// final postulationProvider =
+//     StateNotifierProvider<PostulationProvider, VolunteeringPostulationStatus>((ref) {
+//   return PostulationProvider(ref.read);
+// });
 
-  PostulationProvider(this._read) : super(const PostulationState());
+// class PostulationProvider extends StateNotifier<VolunteeringPostulationStatus> {
+//   final Reader _read;
 
-  Future<void> postulate(String volunteeringId) async {
-    state = state.copyWith(status: PostulationStatus.loading);
-    try {
-      await _read(postulationRepositoryProvider).postulate(volunteeringId);
-      state = state.copyWith(status: PostulationStatus.success);
-    } catch (e) {
-      state = state.copyWith(status: PostulationStatus.error);
-    }
-  }
+//   PostulationProvider(this._read) : super(const PostulationState());
 
-  Future<void> withdrawPostulation(String volunteeringId) async {
-    state = state.copyWith(status: PostulationStatus.loading);
-    try {
-      await _read(postulationRepositoryProvider)
-          .withdrawPostulation(volunteeringId);
-      state = state.copyWith(status: PostulationStatus.success);
-    } catch (e) {
-      state = state.copyWith(status: PostulationStatus.error);
-    }
-  }
-}
+//   Future<void> postulate(String volunteeringId) async {
+//     state = state.copyWith(status: PostulationStatus.loading);
+//     try {
+//       await _read(postulationRepositoryProvider).postulate(volunteeringId);
+//       state = state.copyWith(status: PostulationStatus.success);
+//     } catch (e) {
+//       state = state.copyWith(status: PostulationStatus.error);
+//     }
+//   }
+
+//   Future<void> withdrawPostulation(String volunteeringId) async {
+//     state = state.copyWith(status: PostulationStatus.loading);
+//     try {
+//       await _read(postulationRepositoryProvider)
+//           .withdrawPostulation(volunteeringId);
+//       state = state.copyWith(status: PostulationStatus.success);
+//     } catch (e) {
+//       state = state.copyWith(status: PostulationStatus.error);
+//     }
+//   }
+// }
