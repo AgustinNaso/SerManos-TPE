5f504f18ab4a6aa659e97714dc76ac4e378eac44
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2023-11-12 16:34:01 -0300

    fix: incomplete profile

--- ser_manos/lib/config/cellules/incomplete_profile.dart
+++ ser_manos/lib/config/cellules/incomplete_profile.dart
@@ -20,71 +20,70 @@ class IncompleteProfile extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body:  Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                const ProfileImage(),
-                const SizedBox(height: 8),
-                SizedBox(
-                  child: Column(
-                    children: [
-                      const Text(
-                        "Volunteer",
-                        style: SermanosTypography.overline(
-                          color: SermanosColors.neutral75,
-                        ),
+        body: Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const ProfileImage(size: 120),
+              const SizedBox(height: 16),
+              SizedBox(
+                child: Column(
+                  children: [
+                    const Text(
+                      "Volunteer",
+                      style: SermanosTypography.overline(
+                        color: SermanosColors.neutral75,
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        user.fullName,
-                        style: const SermanosTypography.subtitle01(
-                          color: SermanosColors.neutral100,
-                        ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      user.fullName,
+                      style: const SermanosTypography.subtitle01(
+                        color: SermanosColors.neutral100,
                       ),
-                      const SizedBox(height: 8),
-                      const Text(
+                    ),
+                    const SizedBox(height: 8),
+                    const Padding(
+                      padding: EdgeInsetsDirectional.symmetric(horizontal: 60),
+                      child: Text(
                         "¡Completá tu perfil para tener acceso a mejores oportunidades!",
                         textAlign: TextAlign.center,
                         style: SermanosTypography.body01(
                           color: SermanosColors.neutral75,
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: const InformationCard(
-                      title: 'title', information: [('test', 'test')]),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SermanosCtaButton(
-                        icon: const Icon(Icons.add),
-                        text: "Completar",
-                        onPressed: () => {
-                            
-                         GoRouter.of(context).pushNamed('editProfile')// TODO: create and pass user
-                        }
-                        ),
+                    ),
                   ],
                 ),
-                const SizedBox(
-                  height: 8,
-                ),
-              ],
-            ),
-          ],
-        ),
-      );
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SermanosCtaButton(
+                      icon: const Icon(Icons.add),
+                      text: "Completar",
+                      onPressed: () => {
+                            GoRouter.of(context).pushNamed('editProfile')
+                            // TODO: create and pass user
+                          }),
+                ],
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
